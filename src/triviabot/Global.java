a142d85565069627b6194e9d7aec8e07e839e65b
AeroSteveO <thesteveo64@gmail.com>
2014-09-11 19:56:30 -0400

    initial bot push, simple setup, lib folder is a bit cluttered, lots of extra as it was just a copy from wheatley

---
+++ src/triviabot/Global.java
@@ -0,0 +1,24 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+
+package triviabot;
+
+import org.pircbotx.PircBotX;
+
+/**
+ *
+ * @author Steve-O
+ * YES GASP I KNOW GLOBAL VARIABLES ARE REALLY NICE SOMETIMES
+ *
+ */
+public class Global {
+    public static String BotOwner = new String(); //Updated in the Main .java file from Setings.XML
+    public static String MainNick = new String(); //Updated in the Main .java file from Setings.XML
+    public static String NickPass = new String(); //Updated in the Main .java file from Setings.XML
+    public static boolean reconnect = true;
+    public static PircBotX bot;
+    public static String commandPrefix = "!";       // Not implemented yet in other functions
+}
--- src/triviabot/TriviaBot.java
+++ src/triviabot/TriviaBot.java
@@ -1,22 +1,87 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 package triviabot;
+
+import org.pircbotx.Configuration;
+import org.pircbotx.PircBotX;
+import org.pircbotx.Configuration.*;
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.ConnectEvent;
+import org.pircbotx.hooks.events.KickEvent;
+import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.managers.BackgroundListenerManager;
 
 /**
  *
  * @author Stephen
  */
-public class TriviaBot {
-
+public class TriviaBot extends ListenerAdapter {
+    
+    @Override
+    public void onMessage(final MessageEvent event) throws Exception {
+// in case something should be done here
+    }
+    @Override
+    // Rejoin on Kick
+    public void onKick(KickEvent event) throws Exception {
+        if (event.getRecipient().getNick().equals(event.getBot().getNick())) {
+            event.getBot().sendIRC().joinChannel(event.getChannel().getName());
+        }
+    }
+    @Override
+    // Set mode +B for Bots
+    public void onConnect(ConnectEvent event) throws Exception {
+        event.getBot().sendRaw().rawLine("mode " + event.getBot().getNick() + " +B");
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Setup this bot
+        BackgroundListenerManager BackgroundListener = new BackgroundListenerManager();
+        
+        Configuration.Builder configuration = new Configuration.Builder()
+                .setName("TriviaBot") //Set the nick of the bot. CHANGE IN YOUR CODE
+                .setLogin("LQ") //login part of hostmask, eg name:login@host
+                .setAutoNickChange(true) //Automatically change nick when the current one is in use
+                .setCapEnabled(true) //Enable CAP features
+                .addAutoJoinChannel("#rapterverse")
+//.addCapHandler(new TLSCapHandler(new UtilSSLSocketFactory().trustAllCertificates(), true))
+                .setAutoReconnect(true)
+                .setMaxLineLength(425)
+                .setListenerManager(BackgroundListener)//Allow for logger background listener
+                .addListener(new TriviaBot())
+//                .addListener(new Ignite())
+//                .addListener(new Laser())
+//                .addListener(new Logger())
+//                .addListener(new RandChan())
+//                .addListener(new Shakespeare())
+//                .addListener(new GameBomb())
+//                .addListener(new GameMasterMind())
+//                .addListener(new GameHangman())
+//                .addListener(new GameOmgword())
+//                .addListener(new GameReverse())
+//                .addListener(new GameAltReverse())
+//                .addListener(new EnglishSayings())
+//                .addListener(new SimplePing())
+                .setServerHostname("irc.stevensnet.info"); //Join the official #pircbotx channel
+        //.buildConfiguration();
+//        BackgroundListener.addListener(new Logger(),true); //Add logger background listener
+        Configuration config = configuration.buildConfiguration();
+        //bot.connect throws various exceptions for failures
+        try {
+            Global.bot = new PircBotX(config);
+            //PircBotX bot = new PircBotX(configuration);
+            //Connect to the freenode IRC network
+            Global.bot.startBot();
+        } //In your code you should catch and handle each exception seperately,
+        //but here we just lump them all togeather for simpliciy
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
-    
 }
