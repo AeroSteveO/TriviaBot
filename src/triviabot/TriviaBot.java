5077f6d5180275d1bf0a5362ad91fb9f20a9d135
AeroSteveO <thesteveo64@gmail.com>
2014-09-12 22:40:30 -0400

    original repo license and text added with some additions

--- src/triviabot/TriviaBot.java
+++ src/triviabot/TriviaBot.java
@@ -1,7 +1,57 @@
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
+/**
+* Copyright (C) 2013 Joe Rawson
+* Java port and included upgrades by Stephen
+* -> Additional functionality and robustness being added in including:
+*    - Rejoin server on network failure
+*    - Rejoin channel on kick
+*    - Fixing issue with ?standings command
+*    - Ability to easily change the prefix to commands
+*    - TRIVIA WARS
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Need to load the scores, if they exist, and connect to irc, displaying
+* a welcome message.
+*
+* Scores should be kept in a class which will hold a nick -> score dict
+* object, and at the end of every question will dump the dict to json
+* where it can be loaded from. This might get weird if people start using
+* weird nicks, but we'll cross that road when we get to it.
+*
+* irc connection should be a class, and we should use twisted. We don't
+* really care if people come and go, since everyone in the channel is
+* playing. Should handle this like karma. Watch all traffic, and if
+* someone blurts out a string that matches the answer, they get the points.
+* If they haven't scored before, add them to the scoreboard and give them
+* their points, else, add their points to their total. Then dump the json.
+*
+* This bot requires there to be a ../questions/ directory with text files
+* in it. These files are named after there genres, so "80s Films.txt"
+* and the like. While the bot is running, it will randomly choose a
+* file from this directory, open it, randomly choose a line, which is
+* a question*answer pair, then load that into a structure to be asked.
+*
+* Once the question is loaded, the bot will ask the IRC channel the
+* question, wait a period of time, show a character, then ask the question
+* again.
+*
+* The bot should respond to /msgs, so that users can check their scores,
+* and admins can give admin commands, like die, show all scores, edit
+* player scores, etc. Commands should be easy to implement.
+*
+* Every so often between questions the bot should list the top ranked
+* players, wait some, then continue.
 */
 
 package triviabot;
