a4a1b220c28a7639deb971747a640b0b3675e2e1
AeroSteveO <thesteveo64@gmail.com>
2014-09-12 22:40:44 -0400

    idea for new functionality

--- src/triviabot/TriviaMain.java
+++ src/triviabot/TriviaMain.java
@@ -1,4 +1,4 @@
-/*
+/**
 * To change this license header, choose License Headers in Project Properties.
 * To change this template file, choose Tools | Templates
 * and open the template in the editor.
@@ -24,8 +24,18 @@ public class TriviaMain extends ListenerAdapter{
         
         
         
-        
-        
+//<theDoctor> Steve-O!!! make a kick function for the channel
+//<theDoctor> !kick burg 
+//<theDoctor> then it starts a trivia that only that person can answer
+//<theDoctor> if they get it right quickly
+//<theDoctor> like reduced time
+//<theDoctor> then it will kick the person
+//<Steve-O> if you don't get the question, you get kicked, if you get the question, they get kicked
+//<theDoctor> trivia WARS
+//<theDoctor> hell, make the stakes higher to use it
+//<theDoctor> 1min ban
+//<theDoctor> for the kicker
+//<theDoctor> no ban on kickee
         
         
     }
