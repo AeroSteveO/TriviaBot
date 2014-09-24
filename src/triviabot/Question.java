29967a7ba4f3de26f4135d71b88a1493609316fd
AeroSteveO <thesteveo64@gmail.com>
2014-09-23 22:44:55 -0400

    new objects, question updater automatically sends out messages with new hints after a certain amount of time, and only a certain amount of hints, question gets the question and parses it

---
+++ src/triviabot/Question.java
@@ -0,0 +1,93 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+
+package triviabot;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import org.pircbotx.hooks.events.MessageEvent;
+
+/**
+ *
+ * @author Steve-O
+ *
+ *
+ */
+public class Question {
+    private String question=null;
+    private String answer=null;
+    private Thread t;
+    QuestionUpdater runnable;
+    
+    public Question(){
+        getNewQuestion();
+    }
+    public Question(MessageEvent event,Answer answer, Question question, int time ){
+        getNewQuestion();
+        QuestionUpdater runnable = new QuestionUpdater(event,answer, this, time);
+        this.t = new Thread(runnable);
+        runnable.giveT(t);
+        t.start();
+    }
+    
+    public void startQuestionUpdates(MessageEvent event,Answer answer, Question question, int time){
+        this.runnable = new QuestionUpdater(event,answer, this, time);
+        this.t = new Thread(runnable);
+        this.runnable.giveT(t);
+        t.start();
+    }
+    public void endQuestionUpdates() throws InterruptedException{
+        this.runnable.end();
+    }
+    public void getNewQuestion(){
+        String[] tmp = loadRandomQuestionFromFile().split("`");
+        this.answer=tmp[1];
+        this.question = tmp[0];
+    }
+    public String getAnswer(){
+        return this.answer;
+    }
+    public String getQuestion(){
+        return this.question;
+    }
+    
+    private String loadRandomQuestionFromFile(){
+        ArrayList<File> fileList = getQuestionFileList();
+        File randomFile = fileList.get((int) (Math.random()*fileList.size()-1));
+        ArrayList<String> questions = loadQuestionFile(randomFile);
+        String randomQuestion = questions.get((int) (Math.random()*questions.size()-1));
+        return randomQuestion;
+    }
+    private ArrayList<File> getQuestionFileList(){
+        File folder = new File("questions/");
+        File[] listOfFilesAndFolders = folder.listFiles();
+        ArrayList<File> listOfFiles = new ArrayList<>();// = new File[];
+        for (int i = 0; i < listOfFilesAndFolders.length; i++) {
+            if (listOfFilesAndFolders[i].isFile()) {
+                listOfFiles.add(listOfFilesAndFolders[i]);
+            } else if (listOfFilesAndFolders[i].isDirectory()) {
+                System.out.println("Directory " + listOfFilesAndFolders[i].getName());
+            }
+        }
+        return listOfFiles;
+    }
+    private ArrayList<String> loadQuestionFile(File file) {
+        try{
+            Scanner wordfile = new Scanner(file);
+            ArrayList<String> wordls = new ArrayList<String>();
+            while (wordfile.hasNext()){
+                wordls.add(wordfile.nextLine());
+            }
+            wordfile.close();
+            return (wordls);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/triviabot/QuestionUpdater.java
@@ -0,0 +1,66 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+
+package triviabot;
+
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.events.MessageEvent;
+
+/**
+ *
+ * @author Steve-O
+ */
+public class QuestionUpdater implements Runnable{
+    private PircBotX bot;
+    private Thread t;
+    private boolean running = false;
+    private String channel = null;
+    private Answer answer;
+    private Question question;
+    private int time;
+    
+    QuestionUpdater(MessageEvent event,Answer answer, Question question, int time){
+        this.bot = event.getBot();
+        this.channel = event.getChannel().getName();
+        this.answer = answer;
+        this.question = question;
+        this.running = true;
+        this.time = time; // Seconds
+    }
+    
+    public void giveT(Thread t) {
+        this.t = t;
+    }
+    public void end() throws InterruptedException{
+        this.running = false;
+        t.join(1000); //Ensure the thread also closes
+    }
+    @Override
+    public void run() {
+        this.bot.sendIRC().message(this.channel,"Question:");
+        this.bot.sendIRC().message(this.channel, this.question.getQuestion());
+        this.bot.sendIRC().message(this.channel,"Clue: "+this.answer.getClue());
+        int counter = 1;
+        try{
+            this.t.sleep(this.time*1000);
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            System.out.println(ex.getMessage());
+        }
+        while (this.running&&counter<4){
+            try {
+//                this.bot.sendIRC().message(this.channel, this.question.getQuestion());
+                this.bot.sendIRC().message(this.channel,"Clue: "+this.answer.giveClue());
+                this.t.sleep(this.time*1000); 
+                counter++; // just to make sure the queue stops before giving out more hints than it should
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
