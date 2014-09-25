15113bec3df6e9d2c247b30be981fee8e1614d75
AeroSteveO <thesteveo64@gmail.com>
2014-09-24 22:06:38 -0400

    comments, and modified some values

--- src/triviabot/Global.java
+++ src/triviabot/Global.java
@@ -12,17 +12,24 @@ import org.pircbotx.PircBotX;
 /**
  *
  * @author Steve-O
- * YES GASP I KNOW GLOBAL VARIABLES ARE REALLY NICE SOMETIMES
+ * botOwner      - User with all powers over the bot, and ability to shut the bot down
+ * mainNick      - The intended nickname for the bot, not necessarily the current nickname
+ * nickPass      - The nickServ password for the bot
+ * botAdmins     - Administrators of the bot, ability to use some commands
+ * commandPrefix - The character that signals the bot that a command is being sent
+ * reconnect     - Boolean to activate the aggressive server reconnect loop
+ * bot           - Current PircBotX bot object
+ * 
  *
  */
 public class Global {
     public static String botOwner = "Steve-O"; //Updated in the Main .java file from Setings.XML
     public static ArrayList<String> botAdmins = getBotAdmins();
-    public static String mainNick = new String(); //Updated in the Main .java file from Setings.XML
-    public static String nickPass = new String(); //Updated in the Main .java file from Setings.XML
+    public static String mainNick = "TriviaBot";     //Updated in the Main .java file from Setings.XML
+    public static String nickPass = ""; //Updated in the Main .java file from Setings.XML
     public static boolean reconnect = true;
     public static PircBotX bot;
-    public static String commandPrefix = "!";       // Not implemented yet in other functions
+    public static String commandPrefix = "!";
     
     private static ArrayList<String> getBotAdmins() {
         ArrayList<String> admins = new ArrayList<>();
