e3655e4ee9c1614ea7e97afa0f43388f581c00d8
AeroSteveO <thesteveo64@gmail.com>
2014-10-02 22:52:26 -0400

    still incomplete, won't update to next question after the second question times out, have to add in the key event send in the question updater and move it out of the timed wait for queue/queue time objects

--- src/TriviaBot/TriviaMain.java
+++ src/TriviaBot/TriviaMain.java
@@ -6,6 +6,9 @@
 
 package TriviaBot;
 
+import Objects.Answer;
+import Objects.Question;
+import Objects.TimedWaitForQueue;
 import Objects.Vote;
 import org.pircbotx.Colors;
 import org.pircbotx.hooks.ListenerAdapter;
@@ -22,9 +25,12 @@ public class TriviaMain extends ListenerAdapter{
     boolean runTrivia = false;                              // Should Trivia Be Running
     int numQuestionsAllowedTillEnd = 5;                     // Number of questions allowed to go without response till trivia ends
     int questionsTillAutoEnd = numQuestionsAllowedTillEnd;  // Number of questions left till trivia ends
-    int votesTillStart = 3; // Number of votes needed to start trivia
-    int key=(int) (Math.random()*100000+1);
-    VoteLog votes = new Vote.VoteLog(); // Log of current votes for starting trivia
+    int timeBetweenUpdates = 5;                            // Seconds between each clue update    //30
+    int time = 20;               // Seconds between the start of the trivia challenge and failure //160
+//    int votesTillStart = 3;       // Number of startVotes needed to start trivia
+    //int key=(int) (Math.random()*100000+1);
+    VoteLog startVotes = new Vote.VoteLog();                     // Log of current Votes for starting trivia
+    VoteLog stopVotes  = new Vote.VoteLog();                     // Log of current Votes for stopping trivia
     
     @Override
     public void onMessage(MessageEvent event) throws InterruptedException, Exception {
@@ -35,21 +41,70 @@ public class TriviaMain extends ListenerAdapter{
                 runTrivia = true;
             }
             else if (command.equalsIgnoreCase("start")){
-                votes.addVote(event.getUser().getNick());
+                startVotes.addVote(event.getUser().getNick());
+            }
+            else if (command.equalsIgnoreCase("stop")){
+                stopVotes.addVote(event.getUser().getNick());
             }
             else if (command.equalsIgnoreCase("end")&&Global.botAdmins.contains(event.getUser().getNick())){
                 runTrivia = false;
             }
-        }
-        
-        
-        runTrivia = votes.start();
-        
-        if (runTrivia){
             
         }
         
         
-        
+//        runTrivia = startVotes.start();
+//        if (!Global.activeGames.isGameActive(event.getChannel().getName())){
+//
+//        }
+        if ((runTrivia||startVotes.start())&&!Global.activeGames.isGameActive(event.getChannel().getName())){
+            runTrivia = false;
+            startVotes.clear();
+            String triviaChan = event.getChannel().getName();
+            Question kickQuestion = new Question();
+            Answer kickAnswer = new Answer(kickQuestion.getAnswer());
+            
+            event.getBot().sendIRC().message(event.getChannel().getName(),"Question:");
+            event.getBot().sendIRC().message(event.getChannel().getName(), kickQuestion.getQuestion());
+            int key=(int) (Math.random()*100000+1);
+            TimedWaitForQueue timedQueue = new TimedWaitForQueue(event,time,key);
+            kickQuestion.startQuestionUpdates(event, kickAnswer, kickQuestion, timeBetweenUpdates);
+            boolean running = true;
+            while (running){
+                MessageEvent CurrentEvent = timedQueue.waitFor(MessageEvent.class);
+                String currentChan = CurrentEvent.getChannel().getName();
+                if (CurrentEvent.getMessage().equalsIgnoreCase(Integer.toString(key))){
+                    event.getBot().sendIRC().message(triviaChan,"No one got it. The answer was: "+Colors.BOLD+Colors.RED+kickQuestion.getAnswer());
+                    
+                    
+//                    running = false;
+                    kickQuestion.endQuestionUpdates();
+//                    timedQueue.end();
+                    kickQuestion.getNewQuestion();
+                    kickAnswer.setAnswer(kickQuestion.getAnswer());
+                    event.getBot().sendIRC().message(triviaChan,"Next Question:");
+                    event.getBot().sendIRC().message(triviaChan,kickQuestion.getQuestion());
+                    kickQuestion.startQuestionUpdates(event, kickAnswer, kickQuestion, timeBetweenUpdates);
+                }
+                else if (CurrentEvent.getMessage().equalsIgnoreCase(kickQuestion.getAnswer())){
+                    
+                    event.getBot().sendIRC().message(triviaChan,CurrentEvent.getUser().getNick()+" GOT IT!");
+                    
+//                    running = false;
+                    kickQuestion.endQuestionUpdates();
+//                    timedQueue.end();
+                    kickQuestion.getNewQuestion();
+                    kickAnswer.setAnswer(kickQuestion.getAnswer());
+                    event.getBot().sendIRC().message(triviaChan,"Next Question:");
+                    event.getBot().sendIRC().message(triviaChan,kickQuestion.getQuestion());
+                    kickQuestion.startQuestionUpdates(event, kickAnswer, kickQuestion, timeBetweenUpdates);
+                }
+                else if (CurrentEvent.getMessage().equalsIgnoreCase(Global.commandPrefix+"stop")){
+                    running = false;
+                    kickQuestion.endQuestionUpdates();
+                    timedQueue.end();
+                }
+            }
+        }
     }
 }
