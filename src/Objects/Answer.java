1ca9e191f748c8519ddf36bf6da31d0961668d65
AeroSteveO <thesteveo64@gmail.com>
2014-10-04 21:20:25 -0400

    fixed issue with unmasking a short answer causing the bot to seize up

--- src/Objects/Answer.java
+++ src/Objects/Answer.java
@@ -1,15 +1,15 @@
 /**
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 package Objects;
 
 /**
  *
- * @author Steve-O    
- *   This class implements storage for an answer you want to conceal 
+ * @author Steve-O
+ *   This class implements storage for an answer you want to conceal
  *   and give clues 1 letter at a time.
  *
  *   Methods:
@@ -19,7 +19,7 @@ package Objects;
  *   setAnswer(String): makes this object reusable, sets a new answer and clue mask.
  *   reveal(): returns the answer string.
  *   mask(String): returns the masked answer
- * 
+ *
  */
 public class Answer {
     String answer=null;
@@ -45,26 +45,61 @@ public class Answer {
         return(blanks);
     }
     public String giveClue(){
-        int charLocation = (int) (Math.random()*this.answer.length()-1);
+//        int charLocation = (int) (Math.random()*this.answer.length()-1);
         boolean charChange = false;
-        while (!charChange){
-            try{
-//                if (!Character.isDigit(maskedAnswer.charAt(charLocation))&&!Character.isLetter(maskedAnswer.charAt(charLocation))){  
-//                }
-                if (this.maskedAnswer.charAt(charLocation)=='*'){
-                    if (charLocation<=answer.length()-2)
-                        this.maskedAnswer=this.maskedAnswer.substring(0,charLocation)+this.answer.charAt(charLocation)+this.maskedAnswer.substring(charLocation+1);
+        int numCharsLeft = 0;
+        for (int i=0;i<this.maskedAnswer.length();i++){
+            if (this.maskedAnswer.charAt(i)=='*'){
+                numCharsLeft++;
+            }
+        }
+        if (numCharsLeft>1){
+//            int charLocation = (int) (Math.random()*numCharsLeft-1);
+            int i=(int) (Math.random()*numCharsLeft-1)-1;
+            if (i<0)
+                i=0;
+            while (i<this.maskedAnswer.length()&&!charChange){
+                if (this.maskedAnswer.charAt(i)=='*'){
+                    if (i<=answer.length()-2)
+                        this.maskedAnswer=this.maskedAnswer.substring(0,i)+this.answer.charAt(i)+this.maskedAnswer.substring(i+1);
                     else
-                        this.maskedAnswer=this.maskedAnswer.substring(0,charLocation)+this.answer.charAt(charLocation);
+                        this.maskedAnswer=this.maskedAnswer.substring(0,i)+this.answer.charAt(i);
                     charChange = true;
                 }
+                i++;
             }
-            catch(Exception ex){
-                ex.printStackTrace();
-                System.out.println(ex.getMessage());
-            }
-            charLocation = (int) (Math.random()*this.answer.length()-1);
         }
+        else{
+            for (int i=0;i<this.maskedAnswer.length();i++){
+                if (this.maskedAnswer.charAt(i)=='*'){
+                    if (i<=answer.length()-2)
+                        this.maskedAnswer=this.maskedAnswer.substring(0,i)+this.answer.charAt(i)+this.maskedAnswer.substring(i+1);
+                    else
+                        this.maskedAnswer=this.maskedAnswer.substring(0,i)+this.answer.charAt(i);
+                    charChange = true;
+                    break;
+                }
+            }
+        }
+//        while (!charChange){
+//            try{
+////                if (!Character.isDigit(maskedAnswer.charAt(charLocation))&&!Character.isLetter(maskedAnswer.charAt(charLocation))){
+////                }
+//                if (this.maskedAnswer.charAt(charLocation)=='*'){
+//                    if (charLocation<=answer.length()-2)
+//                        this.maskedAnswer=this.maskedAnswer.substring(0,charLocation)+this.answer.charAt(charLocation)+this.maskedAnswer.substring(charLocation+1);
+//                    else
+//                        this.maskedAnswer=this.maskedAnswer.substring(0,charLocation)+this.answer.charAt(charLocation);
+//                    charChange = true;
+//                }
+//            }
+//            catch(Exception ex){
+//                ex.printStackTrace();
+//                System.out.println(ex.getMessage());
+//            }
+//            
+//            charLocation = (int) (Math.random()*this.answer.length()-1);
+//        }
         return this.maskedAnswer;
     }
     public String getClue(){
