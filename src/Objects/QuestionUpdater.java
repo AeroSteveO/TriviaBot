0bfcd8de80753fe5985f6a71d03d2affa780951c
AeroSteveO <thesteveo64@gmail.com>
2014-10-12 15:32:22 -0400

    better comments, removed unnecessary system lines, added bit more robustness of sending new clues

--- src/Objects/QuestionUpdater.java
+++ src/Objects/QuestionUpdater.java
@@ -19,10 +19,11 @@ import org.pircbotx.hooks.events.MessageEvent;
  * - Automatically gets a new clue to the question and sends that clue to the channel
  *
  * Methods:
- *     *giveT - Gives a thread to the object
- *     *end   - Ends the automatic updating, isn't an instant stop, but it won't
- *              send any more updates to the channel after this is used
- *     *run   - The heart of the auto-updating, a looped thread
+ *     *giveT    - Gives a thread to the object
+ *     *end      - Ends the automatic updating, isn't an instant stop, but it won't
+ *                 send any more updates to the channel after this is used
+ *     *run      - The heart of the auto-updating, a looped thread
+ *     *getCount - Gets the current counter value for the number of clues given
  *
  * Note: Only commands marked with a * are available for use outside the object
  */
@@ -86,13 +87,11 @@ public class QuestionUpdater implements Runnable{
         }
         System.out.println(this.running);
         while (this.running&&this.counter<4){
-            System.out.println("Bing an update");
-            System.out.println(this.counter+" is the count");
+//            System.out.println("Bing an update");
+//            System.out.println(this.counter+" is the count");
             try {
-//                if (!this.running)
-//                    return;
-                
-                this.bot.sendIRC().message(this.channel,"Clue: "+this.answer.giveClue());
+                if (this.running)
+                    this.bot.sendIRC().message(this.channel,"Clue: "+this.answer.giveClue());
                 Thread.sleep(this.time*1000);
                 this.counter++; // just to make sure the queue stops before giving out more hints than it should
             } catch (Exception ex) {
@@ -102,7 +101,7 @@ public class QuestionUpdater implements Runnable{
         }
         if (key != 0 && this.running){
             bot.getConfiguration().getListenerManager().dispatchEvent(new MessageEvent(Global.bot,event.getChannel(),event.getBot().getUserBot(),Integer.toString(key)));
-            System.out.println("PING PONG");
+//            System.out.println("PING PONG");
         }
 //        counter = 1;
 //        this.running = true;
