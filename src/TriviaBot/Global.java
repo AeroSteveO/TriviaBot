5dcd31baa61a2c26f6e2b874bafb8d6c6a88b9c9
AeroSteveO <thesteveo64@gmail.com>
2015-04-29 21:48:46 -0400

    Removed unnecessary object and code

--- src/TriviaBot/Global.java
+++ src/TriviaBot/Global.java
@@ -8,7 +8,6 @@ package TriviaBot;
 
 import java.util.ArrayList;
 import org.pircbotx.PircBotX;
-import Objects.TriviaGames.TriviaArray;
 
 /**
  *
@@ -27,11 +26,10 @@ public class Global {
     public static String botOwner = "Steve-O"; 
     public static ArrayList<String> botAdmins = getBotAdmins();
     public static String mainNick = "TriviaBot";     
-    public static String nickPass = ""; 
+    public static String nickPass = "dertriviabert"; 
     public static boolean reconnect = true;
     public static PircBotX bot;
     public static String commandPrefix = "!";
-    public static TriviaArray activeGames = new TriviaArray();
     
     private static ArrayList<String> getBotAdmins() {
         ArrayList<String> admins = new ArrayList<>();
