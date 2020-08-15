e5ca92f31af43923357efb606806fb9b65dca5e0
AeroSteveO <thesteveo64@gmail.com>
2020-08-15 17:13:48 -0500

    adding support for java env vars alongside system env vars

--- src/rapternet/irc/bots/triviabot/Env.java
+++ src/rapternet/irc/bots/triviabot/Env.java
@@ -10,12 +10,17 @@ package rapternet.irc.bots.triviabot;
  * @author thest
  */
 public class Env {
-    public static final String CONFIG_LOCATION = System.getenv().getOrDefault("BOT_CONFIG_FOLDER", "/config/");
-    public static final String NICK = System.getenv().getOrDefault("BOT_NICK", "TriviaBot");
-    public static final String PASSWORD = System.getenv().getOrDefault("BOT_PASSWORD", "password");
-    public static final String OWNER_NICK = System.getenv().getOrDefault("BOT_OWNER_NICK", "Steve-O");
-    public static final String IRC_PORT = System.getenv().getOrDefault("BOT_IRC_PORT", "6667");
-    public static final String LOGIN = System.getenv().getOrDefault("BOT_LOGIN", "Derpy");
-    public static final String IRC_ADDRESS = System.getenv().getOrDefault("BOT_IRC_ADDRESS", "irc.rapternet.us");
-    public static final String[] CHANNEL_LIST = System.getenv().getOrDefault("BOT_CHANNEL_LIST", "#testing,#rapterverse").split(",");
+    public static final String CONFIG_LOCATION = getVarOrDefault("BOT_CONFIG_FOLDER", "/config/");
+    public static final String NICK = getVarOrDefault("BOT_NICK", "TriviaBot");
+    public static final String PASSWORD = getVarOrDefault("BOT_PASSWORD", "password");
+    public static final String OWNER_NICK = getVarOrDefault("BOT_OWNER_NICK", "Steve-O");
+    public static final String IRC_PORT = getVarOrDefault("BOT_IRC_PORT", "6667");
+    public static final String LOGIN = getVarOrDefault("BOT_LOGIN", "Derpy");
+    public static final String IRC_ADDRESS = getVarOrDefault("BOT_IRC_ADDRESS", "irc.rapternet.us");
+    public static final String[] CHANNEL_LIST = getVarOrDefault("BOT_CHANNEL_LIST", "#testing,#rapterverse").split(",");
+    
+    static String getVarOrDefault(String key, String def) {
+        String javaProp = System.getProperty(key, def);
+        return System.getenv().getOrDefault(key, javaProp);
+    }
 }
